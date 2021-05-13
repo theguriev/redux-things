295cea32c1cf57470826e8c4b8cdd7f97a1585da
Eugen Guriev <eugen.guriev@doclerholding.com>
2021-05-13 16:53:33 +0300

    chore: clean up

--- packages/core/dist/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.ENTITIES_NAMESPACE = void 0;
-const ENTITIES_NAMESPACE = '@redux-entities';
-exports.ENTITIES_NAMESPACE = ENTITIES_NAMESPACE;
