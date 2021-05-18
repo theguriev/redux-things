c3c103bde1a262c5384f9ad7650b589679318632
Eugen Guriev <gurievcreative@gmail.com>
2021-05-18 21:33:44 +0300

    docs(storybook): add useMounted

--- packages/storybook/.storybook/preview.js
+++ packages/storybook/.storybook/preview.js
@@ -6,7 +6,16 @@ import { setupAsyncReducers, ThingsContext } from '@redux-things/core'
 export const parameters = {
     options: {
         storySort: {
-            order: ['Getting Started', ['Introduction', 'Installation'], 'Concepts', ['Things', 'Mutations'], 'Examples', ['Simple', 'Basic'], 'API Reference', ['useThing', 'useMutation']],
+            order: [
+                'Getting Started',
+                ['Introduction', 'Installation'],
+                'Concepts',
+                ['Things', 'Mutations'],
+                'Examples',
+                ['Simple', 'Basic'],
+                'API Reference',
+                ['useThing', 'useThingContext', 'useMutation', 'useMutationContext']
+            ],
         }
     }
 }
