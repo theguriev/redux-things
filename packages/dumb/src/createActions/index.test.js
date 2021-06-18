e5596ed9d0c23eef87067830c992baed6235e6b5
Eugen Guriev <eugen.guriev@doclerholding.com>
2021-06-18 21:22:24 +0300

    feat(dumb): add createActions

--- packages/dumb/src/createActions/index.test.js
+++ packages/dumb/src/createActions/index.test.js
@@ -1,37 +1,18 @@
 import { createActions } from './index'
 
-const addBufferWithDefaults = () => ({
-    buffer: {
-        hello: 'world'
-    }
-})
-
-const addValuesToBuffer = ({ buffer }) => ({
-    buffer: {
-        ...buffer,
-        values: [1, 2, 3]
-    }
-})
-
-const moveBufferToState = ({ buffer }) => ({
-    state: buffer
-})
-
-const reducer = createReducer({
-    testFlow: [addBufferWithDefaults, addValuesToBuffer, moveBufferToState],
-    testFunction: (_state, _action, extra) => extra
-})
-
 const PREFIX = 'SOME_PREFIX/'
 
 const toType = str => `${PREFIX}${str}`
 
 describe('createActions', () => {
-    it('should create action functions based on object', () => {
-        const actions = createActions({
-            edit: undefined,
-            add: undefined
-        }, toType)
-        expect(actions).toBe(1)
+    it('should create action functions based on array', () => {
+        const actions = createActions(['edit', 'add'], toType)
+        expect(typeof actions.edit).toBe('function')
+        expect(typeof actions.add).toBe('function')
+
+        const generatedAction = actions.edit({ hello: 'world' })
+
+        expect(generatedAction.type).toBe(`${PREFIX}edit`)
+        expect(generatedAction.hello).toBe('world')
     })
 })
