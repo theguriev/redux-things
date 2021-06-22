16595d688fc2be3e7780fe699c7200756dc195d5
Eugen Guriev <gurievcreative@gmail.com>
2021-06-22 22:34:43 +0300

    docs(core): add some comments to useThingContext hook

--- packages/core/src/hooks/useThingContext/hooks.js
+++ packages/core/src/hooks/useThingContext/hooks.js
@@ -2,11 +2,6 @@ import { useContext } from 'react'
 import { ThingContext } from './context'
 import { thingDefaults } from './defaults'
 
-/**
- * Get ThingContext object.
- * @param {object} optionsToMerge any additional option to merge.
- * @returns ThingContext object.
- */
 export const useThingContext = (optionsToMerge = {}) => ({
     ...thingDefaults,
     ...useContext(ThingContext),
